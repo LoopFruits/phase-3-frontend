1f5bcd85ec0fa5d2d555b062223cfcca3eb7d75c
tylergubser <tylergubser2200@gmail.com>
2022-03-29 17:24:48 -0500

    messed with loggin post

--- my-app-frontend/src/CreatePatientLog.js
+++ my-app-frontend/src/CreatePatientLog.js
@@ -3,36 +3,45 @@ import React, { useState } from 'react';
 
 function CreatePatientLog() {
 
-  // const [user, setUser] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  function handleUsernameChange(e){
+    setUsername(e.target.value)
+  }
+
+  function handlePasswordChange(e){
+    setPassword(e.target.value)
+  }
     
     function handleFormSubmit(e) {
       e.preventDefault();
-      const itemData = {
-        username: e.target.value, 
-        password: e.target.value
-      };
+      
+      
       fetch('http://localhost:9292/users', {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
         },
-        body:JSON.stringify(itemData),
-      })
-        // .then((response) => response.json())
-        // .then((newItem) => {
-        //   setUser([...user, newItem])
-          
-        }
+        body:JSON.stringify({
+          username: username,
+          password: password
+        }),
+       
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(data));}
+        
     
         return (
         <div className="body-1">
         <h1>Register Patient</h1>
         <form>
           <div>
-            <input type="text" name="username" placeholder="Username" />
+            <input type="text" name="username" placeholder="Username" onChange={handleUsernameChange} />
           </div>
           <div>
-            <input name="password" placeholder="Password" />
+            <input name="password" placeholder="Password" onChange={handlePasswordChange}/>
           </div>
           <input type="submit" value="Create Account" onSubmit={handleFormSubmit}/>
         </form>
