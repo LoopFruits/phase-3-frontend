9a1b625f9eb6aa7f9c293909e8ca3244405ccc71
Lyssie <lyssiewright.15@gmail.com>
2022-03-29 15:50:27 -0500

    add patient and physician form

--- my-app-frontend/src/Patient.js
+++ my-app-frontend/src/Patient.js
@@ -1,35 +1,136 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Patient() {
-// const [form, setForm] = useState([]);
 
-//   function handleSubmit() {
-  
-// }
+    const [lastName, setLastName] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [email, setEmail] = useState("")
+    const [phone, setPhone] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [idNumber, setIdNumber] = useState("")
+
+    //const [formData, setFormData] = useState({})
+
+    function handleFirstNameChange(e){
+     setFirstName(e.target.value)
+    }
+
+    function handleLastNameChange(e){
+     setLastName(e.target.value)
+    }
+
+    function handleEmailChange(e){
+     setEmail(e.target.value)
+    }
+
+    function handleUsernameChange(e){
+     setUsername(e.target.value)
+    }
+    function handlePhoneChange(e){
+     setPhone(e.target.value)
+    }
+
+    function handlePasswordChange(e){
+      setPassword(e.target.value)
+     }
+
+    function handleIdNumberChange(e){
+     setIdNumber(e.target.value)
+    }
+
+
+    function handleSubmit(e){
+      e.preventDefault();
+      fetch("http://localhost:9292/patients", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name: firstName,
+          last_name: lastName,
+          email: email,
+          phone: phone,
+          username: username,
+          password: password,
+          id_number: idNumber
+          }),
+        })
+      .then((r) => r.json())
+      .then((newPatient) => console.log(newPatient));
+      
+         }
+
+
+
 return (
     <div className="body-1">
     <h1>Patient Sign-up</h1>
-    <form>
+    <form onSubmit={handleSubmit}>
         <label>
           <p>First Name</p>
-          <input firstName="firstName" />
+          <input
+            type="text"
+            name="firstName"
+            value={firstName}
+            onChange={handleFirstNameChange}
+          />
         </label>
         <label>
           <p>Last Name</p>
-          <input lastName="lastName" />
+          <input 
+              type="text"
+              name="lastName"
+              value={lastName}
+              onChange={handleLastNameChange}
+              />
         </label>
         <label>
           <p>State ID</p>
-          <input stateId="stateId" />
+          <input  
+              type="text"
+              name="idNumber"
+              value={idNumber}
+              onChange={handleIdNumberChange}
+            />
         </label>
         <label>
-          <p>User Name</p>
-          <input userName="userName" />
-        </label>
-        <label>
-          <p>Password</p>
-          <input password="password" />
-        </label>
+          <p>E-mail</p>
+            <input  
+              type="text"
+              name="email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </label>
+          <label>
+            <p>Phone Number</p>
+              <input  
+                type="text"
+                name="phone"
+                value={phone}
+                onChange={handlePhoneChange}
+            />
+          </label>
+          <label>
+            <p>Username</p>
+              <input  
+                type="text"
+                name="username"
+                value={username}
+                onChange={handleUsernameChange}
+              />
+          </label>
+          <label>
+            <p>Password</p>
+              <input  
+                type="text"
+                name="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+          </label>
       <button type="submit" >Submit</button>
     </form>
   </div>
--- my-app-frontend/src/Physician.js
+++ my-app-frontend/src/Physician.js
@@ -1,39 +1,119 @@
+import React, { useState } from 'react';
+
 function Physician() {
-
+  
+    const [lastName, setLastName] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [country, setCountry] = useState("")
+    const [licenseNumber, setLicenseNumber] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+  
+    function handleFirstNameChange(e){
+      setFirstName(e.target.value)
+    }
+  
+    function handleLastNameChange(e){
+      setLastName(e.target.value)
+    }
+  
+    function handleCountryChange(e){
+      setCountry(e.target.value)
+    }
+  
+    function handleLicenseChange(e){
+      setLicenseNumber(e.target.value)
+    }
+  
+    function handleUsernameChange(e){
+      setUsername(e.target.value)
+    }
+  
+    function handlePasswordChange(e){
+      setPassword(e.target.value)
+    }
+    function handleSubmit(e){
+      e.preventDefault();
+      fetch("http://localhost:9292/physicians", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name: firstName,
+          last_name: lastName,
+          country: country,
+          license_number: licenseNumber,
+          username: username,
+          password: password,
+        }),
+      })
+        .then((r) => r.json())
+        .then((newPhysician) => console.log(newPhysician));
+  
+    }
+  
+  
+  
+  
     return (
-        <div className="body-1">
-    <h1>Physician Sign-up</h1>
-    <form>
-        <label>
-          <p>First Name</p>
-          <input firstName="firstName" />
-        </label>
-        <label>
-          <p>Last Name</p>
-          <input lastName="lastName" />
-        </label>
-        <label>
-          <p>Country</p>
-          <input country="country" />
-        </label>
-        <label>
-          <p>Medical License</p>
-          <input medicalLicense="medicalLicense" />
-        </label>
-        <label>
-          <p>User Name</p>
-          <input userName="userName" />
-        </label>
-        <label>
-          <p>Password</p>
-          <input password="password" />
-        </label>
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-    )
-    
-}
-
-
-export default Physician;
+      <div className="body-1">
+        <h1>Physician Sign-up</h1>
+          <form onSubmit={handleSubmit}>
+            <label>
+            <p>First Name</p>
+              <input
+                type="text" 
+                name="firstName"
+                value={firstName}
+                onChange={handleFirstNameChange} />
+            </label>
+            <label>
+            <p>Last Name</p>
+              <input
+                type="text" 
+                name="lastName"
+                value={lastName}
+                onChange={handleLastNameChange} />
+            </label>
+            <label>
+            <p>Country</p>
+              <input
+                type="text" 
+                name="country"
+                value={country}
+                onChange={handleCountryChange} />
+            </label>
+            <label>
+            <p>Medical License</p>
+              <input 
+                type="text"
+                name="licenseNumber"
+                value={licenseNumber}
+                onChange={handleLicenseChange} />
+            </label>
+            <label>
+            <p>User Name</p>
+              <input
+                type="text" 
+                name="userName"
+                value={username}
+                onChange={handleUsernameChange} />
+            </label>
+            <label>
+            <p>Password</p>
+              <input 
+                type="text" 
+                name="password"
+                value={password}
+                onChange={handlePasswordChange} />
+          </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+      )
+      
+  }
+  
+  
+  export default Physician;
