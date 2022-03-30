4a10c30e4b34abe096c67dbc671c601b71b5ddc9
tylergubser <tylergubser2200@gmail.com>
2022-03-30 16:10:26 -0500

    added edit patient data

---
+++ my-app-frontend/src/Appointment.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+
+
+
+// function Appointment() {
+
+
+// // return(
+
+// //     <div>
+// //         <h2>Name:{patient.name}</h2>
+// //         <h2>Physician Name:{physician.name}</h2>
+// //         <h3>Appointment Date:{appointment.date}</h3>
+// //         <div>
+// //             <button className="sub-btn">Cancel Appointment</button>
+// //         </div>
+// //     </div>
+// // )
+
+// }
+
+// export default Appointment;
--- my-app-frontend/src/EditPatient.js
+++ my-app-frontend/src/EditPatient.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 
 function EditPatient() {
@@ -9,91 +9,113 @@ function EditPatient() {
   const [phone, setPhone] = useState("")
   const [idNumber, setIdNumber] = useState("")
 
+  const history = useHistory();
+  
+    function handleFirstNameChange(e){
+      setFirstName(e.target.value)
+    }
 
-  function handleFirstNameChange(e){
-    setFirstName(e.target.value)
-   }
+    function handleLastNameChange(e){
+      setLastName(e.target.value)
+    }
 
-   function handleLastNameChange(e){
-    setLastName(e.target.value)
-   }
-
-   function handleEmailChange(e){
-    setEmail(e.target.value)
-   }
+    function handleEmailChange(e){
+      setEmail(e.target.value)
+    }
 
   
-   function handlePhoneChange(e){
-    setPhone(e.target.value)
-   }
+    function handlePhoneChange(e){
+      setPhone(e.target.value)
+    }
 
    
 
-   function handleIdNumberChange(e){
-    setIdNumber(e.target.value)
+    function handleIdNumberChange(e){
+      setIdNumber(e.target.value)
+    }
+
+   function componentDidMount() {
+    window.scrollTo(0, 0);
    }
    
-   function handleSubmit() {
+    function handleSubmit(e,id) {
+        e.preventDefault();
+        fetch(`http://localhost:9292/patients/${id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            phone: phone,
+            id_number: idNumber
+            }),
+          })
+        .then((r) => r.json())
+        .then((newPatient) => console.log(newPatient));
+          history.push('/patient-landing');
+          componentDidMount();
+        }
 
-
-   }
+   
         
     
-        return (
-          <div className="body-1">
-          <h1>Edit Information</h1>
-          <form onSubmit={handleSubmit}>
-              <label>
-                <p>First Name</p>
-                <input
-                  type="text"
-                  name="firstName"
-                  value={firstName}
-                  onChange={handleFirstNameChange}
-                />
-              </label>
-              <label>
-                <p>Last Name</p>
-                <input 
-                    type="text"
-                    name="lastName"
-                    value={lastName}
-                    onChange={handleLastNameChange}
-                    />
-              </label>
-              <label>
-                <p>State ID</p>
-                <input  
-                    type="text"
-                    name="idNumber"
-                    value={idNumber}
-                    onChange={handleIdNumberChange}
-                  />
-              </label>
-              <label>
-                <p>E-mail</p>
-                  <input  
-                    type="text"
-                    name="email"
-                    value={email}
-                    onChange={handleEmailChange}
-                  />
-                </label>
-                <label>
-                  <p>Phone Number</p>
-                    <input  
+            return (
+              <div className="body-1">
+              <h1>Edit Information</h1>
+              <form onSubmit={handleSubmit}>
+                  <label>
+                    <p>First Name</p>
+                    <input
                       type="text"
-                      name="phone"
-                      value={phone}
-                      onChange={handlePhoneChange}
-                  />
-                </label>
-               <p>
-            <button type="submit" className="sub-btn" >Submit</button>
-            </p>
-          </form>
-        </div>
-    )
+                      name="firstName"
+                      value={firstName}
+                      onChange={handleFirstNameChange}
+                    />
+                  </label>
+                  <label>
+                    <p>Last Name</p>
+                    <input 
+                        type="text"
+                        name="lastName"
+                        value={lastName}
+                        onChange={handleLastNameChange}
+                        />
+                  </label>
+                  <label>
+                    <p>State ID</p>
+                    <input  
+                        type="text"
+                        name="idNumber"
+                        value={idNumber}
+                        onChange={handleIdNumberChange}
+                      />
+                  </label>
+                  <label>
+                    <p>E-mail</p>
+                      <input  
+                        type="text"
+                        name="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                      />
+                    </label>
+                    <label>
+                      <p>Phone Number</p>
+                        <input  
+                          type="text"
+                          name="phone"
+                          value={phone}
+                          onChange={handlePhoneChange}/>
+                    </label>
+                  <p>
+                <button type="submit" className="sub-btn" >Submit</button>
+                </p>
+              </form>
+            </div>
+          )
 
 
 }
